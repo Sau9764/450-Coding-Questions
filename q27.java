7f0c3d096d61964def2c00abde50b0be7a3aad00
Sourabh Kekade <67153186+Sau9764@users.noreply.github.com>
2021-07-01 12:20:38 +0530

    q27 Added

---
+++ q27.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+class q27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int a[] = new int[n];
+
+        for(int i = 0; i < n; i++) a[i] = sc.nextInt();
+
+        int m = sc.nextInt();
+        int b[] = new int[m];
+
+        for(int i = 0; i < m; i++) b[i] = sc.nextInt();
+
+        System.out.println(isSubset(a, b, n, m));
+
+    }
+
+    public static string isSubset(int a1[], int a2[], int n, int m) {
+        
+    }
+}
